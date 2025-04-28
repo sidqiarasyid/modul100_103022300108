81c113a24d105e0eadc04e6b8d6a2f78a1d253d3
PRAKTIKAN <--unset.all>
2025-04-29 06:55:21 +0700

    wqe

--- Movie.cs
+++ Movie.cs
@@ -1,10 +1,12 @@
 ﻿namespace modul100_103022300108
 {
-    public class Movies
+    public class Movie
     {
         public string Title;
         public string Director;
         public List<String> Stars;
         public string Description;
+
+       
     }
 }
