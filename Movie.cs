af2637e94df5865f22c6f4359ceb46e85e6baf7c
PRAKTIKAN <--unset.all>
2025-04-29 06:53:03 +0700

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace modul100_103022300108
+{
+    public class Movies
+    {
+        public string Title;
+        public string Director;
+        public List<String> Stars;
+        public string Description;
+    }
+}
